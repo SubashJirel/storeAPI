7de037fcd2664eb1556ab30f3082e695b3bd754a
Subash Jirel <subashjrl@gmail.com>
2024-03-24 14:43:18 +0545

    added fields,limit,page,skip features in the products controllers

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,15 @@ const Product = require('../models/product');
 // require('express-async-errors');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === true ? true : false;
@@ -19,12 +23,26 @@ const getAllProducts = async (req, res) => {
   }
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
   }
+
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
