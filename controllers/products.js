5636eb90ca7761fe7252bdd6f4e3b03d20b10cf3
Subash Jirel <subashjrl@gmail.com>
2024-03-24 14:12:58 +0545

    added query search by name,company and sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -2,17 +2,30 @@ const Product = require('../models/product');
 // require('express-async-errors');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find({}).sort('price');
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === true ? true : false;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }; //  $regex and $options are mongodb query operators
+  }
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
 };
